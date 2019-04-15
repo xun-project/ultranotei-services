8fb52b334fc3b15d9b6c6a61783532b338ba85b7
Taegus <taegus.cromis@gmail.com>
2019-04-15 11:09:52 +0200

    complete rewrite of the services module now calls different CG api and supports price, volume and marketcap history parameterized all of the options

--- charts.js
+++ charts.js
@@ -1,108 +1,130 @@
 const { CanvasRenderService } = require('chartjs-node-canvas');
+const queryString = require('query-string');
+const vsprintf = require("sprintf-js").vsprintf;
 const request = require("request");
 const moment = require("moment");
 
-module.exports = {
-  sevenDaysPrice: function (width, height, resultCallback) {
-    function makeConfiguration(data) {
-      var timeLabels = [];
-      var dataLength = data.market_data.sparkline_7d.price.length;
-      var durationAsMS = moment.duration(7 / dataLength, 'd').asMilliseconds();
+function getCoinGeckoData(options, callback) {
+  var queryParams = {
+    vs_currency: options.vsCurrency,
+    days: options.days
+  };
 
-      for (i = dataLength - 1; i >= 0; i--) {
-        timeLabels.push(moment().subtract(durationAsMS * (i + 1), 'ms').format('YYYY-MM-DD'));
-      }
+  var packetData = {
+    uri: vsprintf("https://api.coingecko.com/api/v3/coins/conceal/market_chart?%s", [queryString.stringify(queryParams)]),
+    method: "GET",
+    json: true
+  };
 
-      return {
-        type: 'line',
-        data: {
-          labels: timeLabels,
-          datasets: [{
-            data: data.market_data.sparkline_7d.price,
-            backgroundColor: 'rgba(255,165,0,0.2)',
-            fill: true,
-            borderWidth: 0,
-            pointRadius: 1,
-            borderColor: '#FFA500'
-          }]
-        },
-        options: {
-          animation: false,
-          responsive: true,
-          maintainAspectRatio: false,
-          legend: {
-            display: false,
-            labels: {
-              display: false
-            }
-          },
-          scales: {
-            yAxes: [{
-              ticks: {
-                maxTicksLimit: 5,
-                fontSize: 10,
-                callback: function (value, index, values) {
-                  return '$' + value.toFixed(2);
-                }
-              },
-              gridLines: {
-                color: 'rgba(255,255,255,.08)'
-              }
-            }],
-            xAxes: [{
-              ticks: {
-                autoSkip: true,
-                maxTicksLimit: 7,
-                maxRotation: 0,
-                minRotation: 0
-              },
-              gridLines: {
-                color: 'rgba(255, 255, 255, .08)'
-              },
-            }]
-          },
-          hover: {
-            mode: 'nearest',
-            intersect: true
-          },
-          tooltips: {
-            mode: 'index',
-            intersect: false,
-            callbacks: {
-              label: function (tooltipItem) {
-                return "$ " + Number(tooltipItem.yLabel).toFixed(4);
-              }
-            }
-          }
-        }
-      };
+  request(packetData, function (error, response, body) {
+    callback(body);
+  });
+}
+
+function getCustomChart(options, chartData, resultCallback) {
+  function makeConfiguration(data) {
+    var timeLabels = [];
+    var dataPoints = [];
+    var dataLength = data.length;
+    var durationAsMS = moment.duration(options.days / dataLength, 'd').asMilliseconds();
+
+    data.forEach(function (value) {
+      dataPoints.push(value[1]);
+    });
+
+    for (i = dataLength - 1; i >= 0; i--) {
+      timeLabels.push(moment().subtract(durationAsMS * (i + 1), 'ms').format('YYYY-MM-DD'));
     }
 
-    const chartCallback = (Chart) => {
-
-      // Global config example: https://www.chartjs.org/docs/latest/configuration/
-      Chart.defaults.global.elements.rectangle.borderWidth = 2;
-      // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
-      Chart.plugins.register({
-        // plugin implementation
-      });
-      // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
-      Chart.controllers.MyType = Chart.DatasetController.extend({
-        // chart implementation
-      });
+    return {
+      type: 'line',
+      data: {
+        labels: timeLabels,
+        datasets: [{
+          data: dataPoints,
+          backgroundColor: 'rgba(255,165,0,0.2)',
+          fill: true,
+          borderWidth: 0,
+          pointRadius: 1,
+          borderColor: '#FFA500'
+        }]
+      },
+      options: {
+        animation: false,
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+          labels: {
+            display: false
+          }
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              maxTicksLimit: 5,
+              fontSize: 10,
+              callback: function (value, index, values) {
+                return options.priceSymbol + value.toFixed(options.priceDecimals);
+              }
+            },
+            gridLines: {
+              color: 'rgba(255,255,255,.08)'
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit: options.xPoints,
+              maxRotation: 0,
+              minRotation: 0
+            },
+            gridLines: {
+              color: 'rgba(255, 255, 255, .08)'
+            },
+          }]
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true
+        }
+      }
     };
+  }
 
-    var packetData = {
-      uri: "https://api.coingecko.com/api/v3/coins/conceal?sparkline=true",
-      method: "GET",
-      json: true
-    };
+  const chartCallback = (Chart) => {
+    // Global config example: https://www.chartjs.org/docs/latest/configuration/
+    Chart.defaults.global.elements.rectangle.borderWidth = 2;
+    // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
+    Chart.plugins.register({
+      // plugin implementation
+    });
+    // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
+    Chart.controllers.MyType = Chart.DatasetController.extend({
+      // chart implementation
+    });
+  };
 
-    request(packetData, function (error, response, body) {
-      (async () => {
-        const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
-        resultCallback(await canvasRenderService.renderToBuffer(makeConfiguration(body)));
-      })();
+  (async () => {
+    const canvasRenderService = new CanvasRenderService(options.width, options.height, chartCallback);
+    resultCallback(await canvasRenderService.renderToBuffer(makeConfiguration(chartData)));
+  })();
+}
+
+module.exports = {
+  getPriceChart: function (options, resultCallback) {
+    getCoinGeckoData(options, function (data) {
+      getCustomChart(options, data.prices, resultCallback);
+    });
+  },
+  getVolumeChart: function (options, resultCallback) {
+    getCoinGeckoData(options, function (data) {
+      getCustomChart(options, data.total_volumes, resultCallback);
+    });
+  },
+  getMarketcapChart: function (options, resultCallback) {
+    getCoinGeckoData(options, function (data) {
+      getCustomChart(options, data.market_caps, resultCallback);
     });
   }
 };
--- index.js
+++ index.js
@@ -23,9 +23,64 @@ app.listen(configOpts.server.port, () => {
   console.log("Server running on port " + configOpts.server.port);
 });
 
+function getChartOptions(req) {
+  return {
+    vsCurrency: req.query.vsCurrency || 'usd',
+    days: parseInt(req.query.days) || 7,
+    xPoints: parseInt(req.query.xPoints) || 7,
+    priceDecimals: parseInt(req.query.priceDecimals) || 2,
+    priceSymbol: req.query.priceSymbol || "$",
+    width: parseInt(req.query.width) || 1200,
+    height: parseInt(req.query.height) || 400
+  };
+}
+
 // get request for the list of all active nodes
 app.get("/charts/7daysPrice.png", (req, res) => {
-  charts.sevenDaysPrice(parseInt(req.query.width) || 1200, parseInt(req.query.height) || 400, function (image) {
+  charts.getPriceChart(getChartOptions(req), function (image) {
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': image.length
+    });
+
+    res.end(image);
+  });
+});
+
+app.get("/charts/price.png", (req, res) => {
+  var chartOptions = {
+    vsCurrency: req.query.vsCurrency || 'usd',
+    days: parseInt(req.query.days) || 7,
+    xPoints: parseInt(req.query.xPoints) || 7,
+    priceDecimals: parseInt(req.query.priceDecimals) || 2,
+    priceSymbol: req.query.priceSymbol || "$",
+    width: parseInt(req.query.width) || 1200,
+    height: parseInt(req.query.height) || 400
+  };
+
+  charts.getPriceChart(getChartOptions(req), function (image) {
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': image.length
+    });
+
+    res.end(image);
+  });
+});
+
+app.get("/charts/volume.png", (req, res) => {
+  charts.getVolumeChart(getChartOptions(req), function (image) {
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': image.length
+    });
+
+    res.end(image);
+  });
+});
+
+app.get("/charts/marketcap.png", (req, res) => {
+  charts.getMarketcapChart(getChartOptions(req), function (image) {
     res.writeHead(200, {
       'Content-Type': 'image/png',
       'Content-Length': image.length
