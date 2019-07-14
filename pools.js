6a807aa8e8114bbf129091db86bb38d6047ce649
Taegus <taegus.cromis@gmail.com>
2019-07-14 20:10:34 +0200

    shuffle the pools

--- pools.js
+++ pools.js
@@ -1,12 +1,23 @@
 const fs = require("fs");
 
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
 module.exports = {
   getPoolList: function (resultCallback) {
     fs.readFile('./data/pools.json', 'utf8', function (err, data) {
       if (err) {
         throw err;
       } else {
-        resultCallback(JSON.parse(data));
+        resultCallback(shuffle(JSON.parse(data).ccx));
       }
     });
   }
